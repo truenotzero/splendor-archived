d2709af9337eb51aa05192c077e9c30ff80afe0b
truenotzero <truenotzero@gmail.com>
2020-06-03 10:14:40 +0300

    Designed auth API 1. Currently not implemented 2. Choose database: Ecto + DBMS / MNesia

---
+++ apps/auth/lib/auth.ex
@@ -0,0 +1,15 @@
+defmodule Auth do
+  @moduledoc """
+  Documentation for `Auth`.
+  """
+
+
+  @doc """
+  Initiates a request to authenticate a user
+  """
+  def auth(username, password) do
+    # return {:ok, id} if successful
+    # return :error if unsuccessful
+    :error
+  end
+end
